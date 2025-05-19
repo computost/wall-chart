d412688386a8f4b3af1335f8846cd0479f87917c
Melody Universe <melody@computost.com>
2025-05-19 09:50:39 -0700

    lift front-end tests into own profile

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,18 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: "jsdom",
     exclude: ["app/**/*.e2e.test.ts"],
-    include: ["app/**/*.test.{ts,tsx}"],
-    setupFiles: ["vitest/register-jest-dom.ts", "vitest/cleanup-after-each.ts"],
+    projects: [
+      {
+        extends: true,
+        test: {
+          environment: "jsdom",
+          include: ["app/**/*.client.test.{ts,tsx}"],
+          name: "client",
+          setupFiles: ["vitest/register-jest-dom.ts"],
+        },
+      },
+    ],
+    setupFiles: ["vitest/cleanup-after-each.ts"],
   },
 });
