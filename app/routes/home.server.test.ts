8910498d097226ec67df7106bdf862bfcbd2c222
Joshua Hogsett <joshua@computost.com>
2025-05-20 21:33:26 -0700

    mongodb WIP

--- app/routes/home.server.test.ts
+++ app/routes/home.server.test.ts
@@ -1,10 +1,25 @@
+import { Schema } from "mongoose";
 import { describe } from "node:test";
-import { expect, it } from "vitest";
+import { expect } from "vitest";
+
+import { it } from "~/test-utils/test.server";
 
 import { loader } from "./home";
 
 describe("loader", () => {
-  it("should load a message from config", () => {
+  it(
+    "should connect to a database",
+    { timeout: Infinity },
+    async ({ mongoose }) => {
+      const workerSchema = new Schema({ name: String });
+      const Worker = mongoose.model("Worker", workerSchema);
+      const testWorker = new Worker({ name: "Joe" });
+      await testWorker.save();
+      expect(Worker.countDocuments()).toBe(1);
+    },
+  );
+
+  it("should load a message from config", async () => {
     const response = loader({
       context: { VALUE_FROM_EXPRESS: "This is a test" },
       params: {},
