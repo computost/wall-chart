e38e20fc926a544301a29455afe02b6101776b9f
Melody Universe <melody@computost.com>
2025-05-23 13:18:12 -0700

    add background

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
 import { Combobox, ComboboxButton, ComboboxInput } from "@headlessui/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 
+import "./home.css";
+
 export default function Home() {
   return (
-    <div className="h-full min-h-dvh bg-white p-4 dark:bg-black">
+    <div className="topography h-full min-h-dvh bg-white p-4 dark:bg-black">
       <div className="mx-auto h-[812px] w-[375px] rounded-lg bg-stone-50 p-3 shadow-lg shadow-gray-950 dark:bg-stone-950 dark:shadow-gray-50">
         <Combobox>
           <div className="flex gap-2 rounded-md bg-stone-100 p-1 dark:bg-stone-900">
