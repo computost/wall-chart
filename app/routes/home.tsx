ed84b6e8bc72f3bec3c51bffcbc7518ce2f163fd
Melody Universe <melody@computost.com>
2025-05-23 12:18:46 -0700

    remove text shadow

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="h-full min-h-dvh bg-white p-4 dark:bg-black">
       <div className="mx-auto h-[812px] w-[375px] rounded-lg bg-stone-50 p-3 shadow-lg shadow-gray-950 dark:bg-stone-950 dark:shadow-gray-50">
         <Combobox>
-          <div className="flex gap-2 rounded-md bg-stone-100 p-1 shadow shadow-gray-950 dark:bg-stone-900 dark:shadow-gray-50">
+          <div className="flex gap-2 rounded-md bg-stone-100 p-1 dark:bg-stone-900">
             <ComboboxButton>
               <MagnifyingGlassIcon className="size-4" />
             </ComboboxButton>
