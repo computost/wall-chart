65699a6c4615e02be325248833f1654604976f66
Melody Universe <melody@computost.com>
2025-05-23 00:53:35 -0700

    create home screen

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,19 @@
+import { Combobox, ComboboxButton, ComboboxInput } from "@headlessui/react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
-  return <p>Hello, World!</p>;
+  return (
+    <div className="mx-auto mt-4 h-[812px] w-[375px] rounded-lg bg-stone-950 p-3 shadow shadow-gray-50">
+      <Combobox>
+        <div className="flex gap-2 rounded-md bg-green-950 p-1">
+          <ComboboxButton>
+            <MagnifyingGlassIcon className="h-4 w-4" />
+          </ComboboxButton>
+          <ComboboxInput className="grow focus:outline-0" />
+        </div>
+      </Combobox>
+    </div>
+  );
 }
 
 export function meta() {
