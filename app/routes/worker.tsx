159314441ec704021ce3102f6d0a09646e4cc3e3
Melody Universe <melody@computost.com>
2025-05-23 21:54:37 -0700

    add back button from workers page

--- app/routes/worker.tsx
+++ app/routes/worker.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from "react";
 
+import { Button } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/16/solid";
+import { useNavigate } from "react-router";
+
 import { getWorker } from "~/api/workers.server";
 import { Screen } from "~/components/screen";
 
@@ -12,9 +16,16 @@ export function loader({ params }: Route.LoaderArgs) {
 export default function Worker({
   loaderData: worker,
 }: Route.ComponentProps): ReactNode {
+  const navigate = useNavigate();
+
   return (
     <Screen>
-      <p>{worker.name}</p>
+      <div className="flex gap-2 p-1">
+        <Button className="cursor-pointer" onClick={() => navigate("/")}>
+          <XMarkIcon className="size-4" />
+        </Button>
+        <h1>{worker.name}</h1>
+      </div>
     </Screen>
   );
 }
